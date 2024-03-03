99d47ee1c3439119b4adfc12709e1c4606422380
jguo55 <148511131+jguo55@users.noreply.github.com>
2024-03-03 01:37:08 -0800

    experimenting with albums algorithm
    unfortunately spotify does not populate album genre data, pivoting to artist genre data

--- public/server.js
+++ public/server.js
@@ -11,16 +11,29 @@ const getHashParams = () => {
   return hashParams;
 };
 
+function getSeveralAlbums(query){
+  $.ajax({
+    url:`${SPOTIFY_ROOT}/albums?ids=${query}`,
+    headers: {
+      Authorization: 'Bearer ' + access_token,
+    },
+    success: function (data) {
+      return data;
+    }
+  })
+}
+
 let params = getHashParams();
 
 let access_token = params.access_token;
-
 
 
 const generate = () => document.getElementById('generate');
 
 const gen = () => {
   const limit = 50;
+  const tracks = {};
+  let albums = {};
   $.ajax({
     url: `${SPOTIFY_ROOT}/me/top/tracks?limit=${limit}&time_range=short_term`,
     headers: {
@@ -28,9 +41,38 @@ const gen = () => {
     },
     success: function (data) {
       console.log(data);
+      for(let i = 0; i < 50; i++){
+        let score = 50-i;
+        tracks[data.items[i].name] = score;
+
+        if(!(data.items[i].album.id in albums)){
+          albums[data.items[i].album.id] = 0;
+        }
+        albums[data.items[i].album.id] += score;
+
+        //data.items[i].album.name.toString();
+      }
+      
+      let entries = Object.entries(albums)
+      let sorted = entries.sort((a,b) => b[1]-a[1])
+      let ids = []
+      for(let j = 0; j < 20; j++){
+        ids.push(sorted[j][0])
+      }
+      console.log(ids.toString())
+      $.ajax({
+        url:`${SPOTIFY_ROOT}/albums?ids=${ids.toString()}`,
+        headers: {
+          Authorization: 'Bearer ' + access_token,
+        },
+        success: function (data) {
+          console.log(data.albums[0].genres)
+        }
+      })
     }
   })
-    console.log(access_token);
+    console.log(tracks);
+    console.log(albums);
 }
 
 generate().addEventListener('click', gen)
